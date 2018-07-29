9c81cc3d5c64afa7cdb7392b84e0e5099d325af8
taghreedhassan25 <30810540+taghreedhassan25@users.noreply.github.com>
2018-07-29 14:37:18 -0700

    Add files via upload

---
+++ RandomMovieViewModal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+namespace Vidly.ViewModals
+    
+{
+    public class RandomMovieViewModal
+    {
+        public Movie Movie { set; get; }
+        public List<Customer> Customers { set; get; }
+
+    }
+}
